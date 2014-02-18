2bebed12fe64f7d618a2866797c48d3eb89d4c1a
Jeroen Andries <andries.j@live.be>
2014-02-18 11:58:01 +0100

    post command works

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 </head>
 <body>
 <h1>Registratie</h1>
-<form>
+<form action="result.php" method="post">
 	<p>naam:<input type="text" name="name" size="15" maxlength="30">
 	</p>
 	<p>gebruikersnaam:<input type="text" name="username" size="15" maxlength="30">
@@ -14,7 +14,6 @@
 	</p>
 <input type="submit" name="send"
 value="verzenden" />
-
 
 </form>
 </body>
---
+++ result.php
@@ -0,0 +1,5 @@
+<?php 
+echo "Welkom " . htmlspecialchars_decode($_POST["name"])."." ."<br/>";
+
+echo "gebuikersnaam: " . htmlspecialchars_decode($_POST["username"])
+ ?>
