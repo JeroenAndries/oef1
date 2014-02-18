1072731d834f061cc91e69f63769b8a8be8e0777
Jeroen Andries <andries.j@live.be>
2014-02-18 11:36:12 +0100

    layout html made

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<h1>Registratie</h1>
+<form>
+	<p>naam:<input type="text" name="name" size="15" maxlength="30">
+	</p>
+	<p>gebruikersnaam:<input type="text" name="username" size="15" maxlength="30">
+	</p>
+	<p>wachtwoord: <input type="password" name="password" size="15" maxlength="30">
+	</p>
+<input type="submit" name="send"
+value="verzenden" />
+
+
+</form>
+</body>
+</html>
