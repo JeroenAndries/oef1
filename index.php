ce260011662480074b05a6239e7ab953ae64d729
Jeroen Andries <andries.j@live.be>
2014-03-10 11:29:57 +0100

    php with 1 file

--- index.php
+++ index.php
@@ -1,20 +1,54 @@
 <!DOCTYPE html>
+<?php 
+	$name = "";
+	$username = "";
+	$password = "";
+
+	if (isset($_POST["name"])) {
+		$name = $_POST["name"];
+	}
+	if (isset($_POST["username"])) {
+		$username = $_POST["username"];
+	}
+	if (isset($_POST["password"])) {
+		$password = $_POST["password"];
+	}
+	
+
+	$show_result = !empty($name) && !empty($username) && !empty($password);
+
+ ?>
+
 <html>
 <head>
 	<title></title>
 </head>
 <body>
 <h1>Registratie</h1>
-<form action="result.php" method="post">
-	<p>naam:<input type="text" name="name" size="15" maxlength="30">
+
+<?php if ($show_result  ){ ?>
+
+<h1>gegevens:</h1>
+<p>naam ==> <?php echo $name ?>  </p>
+<p>gebruikersnaam ==> <?php echo $username ?>  </p>
+<p>gebruikersnaam ==> <?php echo $password ?>  </p>
+	
+
+<?php }else{ ?>
+<form method="post">
+	<p>naam:<input type="text" value="<?php echo $name ?>" name="name" size="15" maxlength="30">
 	</p>
-	<p>gebruikersnaam:<input type="text" name="username" size="15" maxlength="30">
+	<p>gebruikersnaam:<input type="text" value="<?php echo $username ?>" name="username" size="15" maxlength="30">
 	</p>
-	<p>wachtwoord: <input type="password" name="password" size="15" maxlength="30">
+	<p>wachtwoord: <input type="password" value="<?php echo $password ?>" name="password" size="15" maxlength="30">
 	</p>
 <input type="submit" name="send"
 value="verzenden" />
 
 </form>
+
+
+<?php } ?>
+
 </body>
 </html>
