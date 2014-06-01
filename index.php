8792cc703e0dafb096a101415d966e3b6df2002b
Jeroen Andries <andries.j@live.be>
2014-06-02 00:36:50 +0200

    really working now

--- index.php
+++ index.php
@@ -13,9 +13,18 @@
 	if (isset($_POST["password"])) {
 		$password = $_POST["password"];
 	}
-	
+	if (isset($_GET["g_name"])) {
+		$nameget = $_GET["g_name"];
+	}
 
-	$show_result = !empty($name) && !empty($username) && !empty($password);
+	if (isset($_GET["g_username"])) {
+		$usernameget = $_GET["g_username"];
+	}
+	if (isset($_GET["g_password"])) {
+		$passwordget = $_GET["g_password"];
+	}
+
+	$show_result = !empty($name) && !empty($username) && !empty($password)&& !empty($g_name) && !empty($g_username) && !empty($g_password);
 
  ?>
 
@@ -39,11 +48,20 @@
 
 <div class="container">
 <link rel="stylesheet" type="text/css" href="css\signin.css">
-      <form class="form-signin" method="POST" role="form">
+      <form class="form-signin" method="POST" role="form"action="result.php">
         <h2 class="form-signin-heading">Please sign in</h2>
         <input name="name" type="name" class="form-control" placeholder="naam" required="" autofocus="">
         <input name="username" type="name" class="form-control" placeholder="gebruikersnaam" required="" >
         <input  name="password"type="password" class="form-control" placeholder="wachtwoord" required="">
+        
+        <button class="btn btn-lg btn-primary btn-block" type="submit" name="send" value="verzenden">verzend </button>
+      </form>
+
+<form class="form-signin" method="GET" role="form"action="result.php">
+        <h2 class="form-signin-heading">Please sign in</h2>
+        <input name="g_name" type="name" class="form-control" placeholder="naam" required="" autofocus="">
+        <input name="g_username" type="name" class="form-control" placeholder="gebruikersnaam" required="" >
+        <input  name="g_password"type="password" class="form-control" placeholder="wachtwoord" required="">
         
         <button class="btn btn-lg btn-primary btn-block" type="submit" name="send" value="verzenden">verzend </button>
       </form>
--- result.php
+++ result.php
@@ -1,5 +1,40 @@
-<?php 
-echo "Welkom " . htmlspecialchars_decode($_POST["name"])."." ."<br/>";
+<!DOCTYPE html>
+<html>
+<head>
+	<title>"result post/get</title>
+		<link rel="stylesheet" type="text/css" href="css\bootstrap.css">
+</head>
+<body>
+<?php if (isset($_POST["name"])){ ?>
+<div class="container">
+<link rel="stylesheet" type="text/css" href="css\signin.css">
+      <form class="form-signin" method="POST" role="form">
+        <h2 class="form-signin-heading">Result POST</h2>
+        <input name="name" type="name" class="form-control" value="<?php echo $_POST["name"]?>" disabled >
+        <input name="username" type="name" class="form-control" value="<?php echo $_POST["username"]?>" disabled >
+        <input name="password"type="input" class="form-control" value="<?php echo $_POST["password"]?>" disabled>
+        
+        
+      </form>
+</div>
+	
 
-echo "gebuikersnaam: " . htmlspecialchars_decode($_POST["username"])
- ?>
+<?php }
+if(isset($_GET["g_name"])){ ?>
+
+<div class="container">
+<link rel="stylesheet" type="text/css" href="css\signin.css">
+      
+<form class="form-signin" method="GET" role="form">
+        <h2 class="form-signin-heading">Result GET</h2>
+        <input name="g_name" type="name" class="form-control" value="<?php echo $_GET["g_name"]?>" disabled >
+        <input name="g_username" type="name" class="form-control" value="<?php echo $_GET["g_username"]?>" disabled >
+        <input name="g_password"type="input" class="form-control" value="<?php echo $_GET["g_password"]?>" disabled>
+        
+        
+      </form>
+
+    </div>
+    <?php } ?>
+</body>
+</html>
